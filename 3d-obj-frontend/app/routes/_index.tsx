eb16c416c5599165265e98c111977c16c76f1dae
minoka kakizaki <minoka.kakizaki@gmail.com>
2023-10-25 10:43:05 -0700

    tring to set up git hub oauth

--- 3d-obj-frontend/app/routes/_index.tsx
+++ 3d-obj-frontend/app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { useEffect, useState } from "react"
 
 export const meta: MetaFunction = () => {
   return [
@@ -6,6 +7,8 @@ export const meta: MetaFunction = () => {
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
+
+
 const CLIENT_ID = "926da06118abf355444f"
 
 const loginWithGitHub = (clientId: string) => {
@@ -13,6 +16,38 @@ const loginWithGitHub = (clientId: string) => {
 }
 
 export default function Index() {
+
+  const [rerender, setRerender] = useState(false)
+
+  useEffect(()=> {
+    const queryString = window.location.search
+    const urlParams = new URLSearchParams(queryString)
+    const codeParam = urlParams.get('code')
+    console.log('codeParam', codeParam)
+    console.log(localStorage.getItem('accessToken'))
+    
+    if( codeParam && (localStorage.getItem("accessToken")) == null) {
+      async function getAccessToken() {
+        await fetch(`http://localhost:4000/getAccessToken?code=${codeParam}`, {
+          method: "GET"
+        }).then((response) => {
+          return response.json()
+        }).then((data) => {
+          console.log( data + " this is the data in the front end")
+          if(data.access_token){
+            localStorage.setItem("accessToken", data.access_token)
+            setRerender(!rerender)
+          }
+        })
+      }
+
+      getAccessToken()
+
+    }
+  }, [])
+
+
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
